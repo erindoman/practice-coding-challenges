661cd4112db8037d688830dfe5becce87bbda313
erindoman <erin.doman@gmail.com>
2021-02-16 19:52:36 -0600

    solve 2 easy (python and js)

--- exercises.js
+++ exercises.js
@@ -1,7 +1,18 @@
 // Easy
+// 1. Reverse Integer (JS)
 
+var reverse = function(x) {
+    const n = parseInt(String(Math.abs(x)).toString().split('').reverse().join(''))
+    return n > Math.pow(2,31) - 1 ? 0 : x > 0 ? n : n * - 1  
+}
+
+// 2. Palindrome Number (JS)
+
+// ------------------------------------------------
 
 // Medium
 
+// ------------------------------------------------
 
 // Hard 
+
---
+++ exercises.py
@@ -0,0 +1,15 @@
+# Easy
+# 1. Palindrome Number
+
+class Solution(object):
+    def isPalindrome(self, x):
+        return False if x < 0 else x == int(str(x)[::-1])
+    
+# ------------------------------------------------
+
+# Medium
+
+# ------------------------------------------------
+
+# Hard 
+
